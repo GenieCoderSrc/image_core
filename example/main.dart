f517ab950efe213ce8c76ea510d0f33afdf7200d
ShohidulProgrammer <shoikat.cse@gmail.com>
2025-08-17 01:52:08 +0600

    Fix(*): Replaced `UploadFile` with `XFile`.

--- example/main.dart
+++ example/main.dart
@@ -3,12 +3,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_core/image_core.dart';
 
-
 Future<void> main() async {
-  final File file =  File('asset/example.jpg');
+  final File file = File('asset/example.jpg');
 
   UploadFile? uploadFile = await file.toUploadFileFromFile();
 
   debugPrint('Main | main | uploadFile: ${uploadFile.toString()}');
-
 }
